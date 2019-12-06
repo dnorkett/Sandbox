cb435fc33313fd765bc09e6d1a72cf8887d118da
dnorkett <dnorkett@gmail.com>
2019-12-05 21:18:45 -0600

    Added sample comment

--- Recursion.py
+++ Recursion.py
@@ -1,5 +1,5 @@
 def mult_iter(a,b):
-    """Performs multiplication by adding variable a to itself b times"""
+    """Performs iterative multiplication by adding variable a to itself b times"""
     result = 0
     while b > 0:
         result+=a
@@ -7,12 +7,14 @@ def mult_iter(a,b):
     return result
 
 def mult(a,b):
+    """Performs recursive multiplication"""
     if b == 1:
         return a
     else:
         return a + mult(a, b-1)
 
 def fact(n):
+    """Performs recursive factorial"""
     if n == 1:
         return 1
     else:
