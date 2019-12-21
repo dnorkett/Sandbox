9984a689ea66e81d2632f2b3cebaf308ac661cc4
dnorkett <dnorkett@gmail.com>
2019-12-21 17:17:13 -0600

    Added sample comment

--- PS4/ps4a.py
+++ PS4/ps4a.py
@@ -1,40 +1,21 @@
-# Problem Set 4A
-# Name: <your name here>
-# Collaborators:
-# Time Spent: x:xx
-
 def get_permutations(sequence):
     '''
     Enumerate all permutations of a given string
-
-    sequence (string): an arbitrary string to permute. Assume that it is a
-    non-empty string.  
-
-    You MUST use recursion for this part. Non-recursive solutions will not be
-    accepted.
-
+    sequence (string): an arbitrary string to permute.
     Returns: a list of all permutations of sequence
-
-    Example:
-    >>> get_permutations('abc')
-    ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
-
-    Note: depending on your implementation, you may return the permutations in
-    a different order than what is listed here.
     '''
 
-    pass #delete this line and replace with your code here
+    if len(sequence) <= 1:
+        return [sequence]
+    else:
+        permutations = []
+        first_char = sequence[0]
+        next_chars = sequence[1:]
+        permutations_of_subsequence = get_permutations(next_chars)
+        for seq in permutations_of_subsequence:
+            for index in range(len(seq) + 1):
+                new_seq = seq[0:index] + first_char + seq[index:len(seq) + 1]
+                permutations.append(new_seq)
 
-if __name__ == '__main__':
-#    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
-    
-#    # Put three example test cases here (for your sanity, limit your inputs
-#    to be three characters or fewer as you will have n! permutations for a 
-#    sequence of length n)
-
-    pass #delete this line and replace with your code here
-
+        # print(permutations)
+        return permutations
